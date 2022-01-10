ec4b2e5615c3e5afc0b3290151ca9ecb6362d245
mnartstuff <alexgeerartist@gmail.com>
2022-01-10 00:09:39 -0600

    Added modal input form

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+// window.addEventListener('load', () => {
+  "use strict"
+  const inputModal = document.querySelector("#input-modal")
+
+  document.querySelector("#add-button").addEventListener("click", () => {
+    inputModal.classList.add("active")
+  })
+// })();
